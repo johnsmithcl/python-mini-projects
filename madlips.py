c2f2b7ed3232842f444591b6a0f0477043b26cd3
johnsmithcl <129190351+johnsmithcl@users.noreply.github.com>
2024-04-29 20:53:33 +0530

    Create madlips.py
    this is a random project that creates a paragraph using given inputs

---
+++ madlips.py
@@ -0,0 +1,19 @@
+
+print("Title: A Visit to the Cafeteria")
+
+noun = [input("Enter a noun: ") for _ in range(4)]
+plural = [input("Enter a plural noun: ") for _ in range(6)]
+adjective = [input("Enter an adjective: ") for _ in range(2)]
+adverb = input("Enter an adverb: ")
+letter = input("Enter any letter: ")
+body_part = input("Enter any body part: ")
+
+print("On an sunny day,family went to a vet and there was a lot of cats,dogs and ", random.choice(noun), "A person approached the family", random.choice(adjective), "from inside.")
+print("You'll see the rainbow bridge after it rains cats and dogs.", random.choice(adjective), "Today I bought a raincoat and wore it on a sunny day.", random.choice(noun), "The river stole the gods.", random.choice(noun), "You should never take advice from someone who thinks red paint dries quicker than blue paint.", random.choice(plural) + ".")
+print("There's probably enough glass in my cupboard to build an undersea aquarium" + random.choice(plural) + "I’m a living furnace."
+ +random.choice(noun)"."
+print("He watched the dancing piglets with panda bear tummies in the swimming pool, random.choice(plural) + ".")
+print("You're good at English when you know the difference between a man eating chicken and a man-eating chicken.
+", random.choice(plural) + ", alongside low-calorie", random.choice(plural) + ", and eliminate all saturated", random.choice(plural) + ".")
+print("He knew it was going to be a bad day when he saw mountain lions roaming the streets.
+", letter + "-minus.")
